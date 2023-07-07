a5b160afc926fc88069593180c739029b1b6220d
Partridge <anhduc130703@gmail.com>
2023-07-07 18:01:27 +0700

    complete widget in manga info

--- src/app/manga/[id]/page.tsx
+++ src/app/manga/[id]/page.tsx
@@ -9,6 +9,7 @@ import { db } from '@/lib/db';
 import { Loader2 } from 'lucide-react';
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
+import Script from 'next/script';
 import { FC, Suspense } from 'react';
 
 interface pageProps {
@@ -92,117 +93,133 @@ const page: FC<pageProps> = async ({ params }) => {
     }
   }
 
-  let facebook: string | null = null;
-  if (manga.facebookLink) {
-    let fbStr = manga.facebookLink.replaceAll('/', '%2F');
-    fbStr = fbStr.replaceAll(':', '%3A');
-    facebook = fbStr;
-  }
-
   return (
-    <div className="container mx-auto h-full pt-20 space-y-14">
-      <div className="relative h-max">
-        <div className="p-4 max-sm:space-y-4 md:flex md:gap-10">
-          <MangaImage className="h-48 w-full md:w-40" image={manga.image} />
+    <>
+      <div className="container mx-auto h-full pt-20 space-y-14">
+        <div className="relative h-max">
+          <div className="p-4 max-sm:space-y-4 md:flex md:gap-10">
+            <MangaImage className="h-48 w-full md:w-40" image={manga.image} />
 
-          <div className="space-y-1 md:space-y-2">
-            <p className="text-2xl md:text-4xl font-semibold">{manga.name}</p>
-            <p className="text-sm">
-              {manga.author.map((a) => a.name).join(', ')}
-            </p>
-          </div>
-        </div>
-
-        <div className="absolute inset-0 -z-10">
-          <div className="relative h-full w-full">
-            <Image
-              fill
-              priority
-              sizes="0%"
-              src={manga.image}
-              alt="Manga Background Image"
-              className="object-cover blur-sm brightness-[.2] md:brightness-[.3] rounded-md"
-            />
-          </div>
-        </div>
-      </div>
-
-      <div className="p-3 md:p-6 space-y-10 dark:bg-zinc-900/80 rounded-md">
-        <div className="space-y-1">
-          <p className="font-semibold text-lg">Thể loại</p>
-          <TagWrapper>
-            {manga.tags.map((tag, idx) => (
-              <TagContent
-                key={idx}
-                title={`${tag.description ? tag.description : tag.name}`}
-              >
-                {tag.name}
-              </TagContent>
-            ))}
-          </TagWrapper>
-        </div>
-
-        <div className="space-y-2">
-          <p className="font-semibold text-lg">Mô tả</p>
-          <EditorOutput content={manga.description} />
-        </div>
-
-        <Tabs defaultValue="chapter">
-          <TabsList className="space-x-2 dark:bg-zinc-800">
-            <TabsTrigger value="chapter">Chapter</TabsTrigger>
-            <TabsTrigger value="comment">Bình luận</TabsTrigger>
-          </TabsList>
-          <TabsContent
-            value="chapter"
-            className="grid grid-cols-1 md:grid-cols-[.4fr_1fr] lg:grid-cols-[.3fr_1fr] gap-6"
-          >
-            <div className="space-y-4">
-              <Card className="bg-transparent/10">
-                <CardHeader>
-                  {!!manga.creator.image && <UserAvatar user={manga.creator} />}
-                  <CardTitle>{manga.creator.name}</CardTitle>
-                </CardHeader>
-              </Card>
-
-              {discord.code && (
-                <div className="p-1">
-                  <p className="text-lg px-1">
-                    Discord:{' '}
-                    <span className="font-semibold">{discord.name}</span>
-                  </p>
-                  <iframe
-                    height={300}
-                    src={`https://discord.com/widget?id=${discord.id}&theme=dark`}
-                    allowTransparency
-                    allowFullScreen
-                    sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
-                    className="w-full border dark:border-zinc-700 rounded-md"
-                  />
-                </div>
-              )}
-
-              {facebook && (
-                <div className="p-1">
-                  <p className="text-lg px-1">Facebook</p>
-                  <iframe
-                    height={300}
-                    src={`https://www.facebook.com/plugins/page.php?href=${facebook}&tabs=timeline&small_header=false&hide_cover=false&show_facepile=true&lazy=true`}
-                    allowTransparency
-                    allowFullScreen
-                    allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-                    className="w-full"
-                  />
-                </div>
-              )}
+            <div className="space-y-1 md:space-y-2">
+              <p className="text-2xl md:text-4xl font-semibold">{manga.name}</p>
+              <p className="text-sm">
+                {manga.author.map((a) => a.name).join(', ')}
+              </p>
             </div>
+          </div>
 
-            <Suspense fallback={<Loader2 className="h-6 w-6 animate-spin" />}>
-              <ChapterList mangaId={manga.id} />
-            </Suspense>
-          </TabsContent>
-        </Tabs>
+          <div className="absolute inset-0 -z-10">
+            <div className="relative h-full w-full">
+              <Image
+                fill
+                priority
+                sizes="0%"
+                src={manga.image}
+                alt="Manga Background Image"
+                className="object-cover blur-sm brightness-[.2] md:brightness-[.3] rounded-md"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="p-3 md:p-6 space-y-10 dark:bg-zinc-900/80 rounded-md">
+          <div className="space-y-1">
+            <p className="font-semibold text-lg">Thể loại</p>
+            <TagWrapper>
+              {manga.tags.map((tag, idx) => (
+                <TagContent
+                  key={idx}
+                  title={`${tag.description ? tag.description : tag.name}`}
+                >
+                  {tag.name}
+                </TagContent>
+              ))}
+            </TagWrapper>
+          </div>
+
+          <div className="space-y-2">
+            <p className="font-semibold text-lg">Mô tả</p>
+            <EditorOutput content={manga.description} />
+          </div>
+
+          <Tabs defaultValue="chapter">
+            <TabsList className="space-x-2 dark:bg-zinc-800">
+              <TabsTrigger value="chapter">Chapter</TabsTrigger>
+              <TabsTrigger value="comment">Bình luận</TabsTrigger>
+            </TabsList>
+            <TabsContent
+              value="chapter"
+              className="grid grid-cols-1 md:grid-cols-[.4fr_1fr] lg:grid-cols-[.3fr_1fr] gap-6"
+            >
+              <div className="space-y-4">
+                <Card className="bg-transparent/10">
+                  <CardHeader>
+                    {!!manga.creator.image && (
+                      <UserAvatar user={manga.creator} />
+                    )}
+                    <CardTitle>{manga.creator.name}</CardTitle>
+                  </CardHeader>
+                </Card>
+
+                {discord.code && (
+                  <div className="p-1">
+                    <p className="text-lg px-1">
+                      Discord:{' '}
+                      <span className="font-semibold">{discord.name}</span>
+                    </p>
+                    <iframe
+                      height={300}
+                      src={`https://discord.com/widget?id=${discord.id}&theme=dark`}
+                      allowFullScreen
+                      sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+                      className="w-full border dark:border-zinc-700 rounded-md"
+                    />
+                  </div>
+                )}
+
+                {manga.facebookLink && (
+                  <div className="p-1">
+                    <p className="text-lg px-1">Facebook</p>
+                    <div className="relative w-full p-1">
+                      <div
+                        className="fb-page"
+                        data-href={manga.facebookLink}
+                        data-tabs="timeline"
+                        data-height="300"
+                        data-small-header="false"
+                        data-adapt-container-width="true"
+                        data-hide-cover="false"
+                        data-show-facepile="true"
+                      >
+                        <blockquote
+                          cite={manga.facebookLink}
+                          className="fb-xfbml-parse-ignore"
+                        >
+                          <a href={manga.facebookLink}>
+                            <Loader2 className="w-6 h-6 animate-spin" />
+                          </a>
+                        </blockquote>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
+
+              <Suspense fallback={<Loader2 className="h-6 w-6 animate-spin" />}>
+                <ChapterList mangaId={manga.id} />
+              </Suspense>
+            </TabsContent>
+          </Tabs>
+        </div>
       </div>
-    </div>
+      <Script
+        strategy="lazyOnload"
+        async
+        defer
+        crossOrigin="anonymous"
+        src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v17.0"
+      />
+    </>
   );
 };
 

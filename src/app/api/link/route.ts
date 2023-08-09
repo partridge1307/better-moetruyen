b1a1ea63198a4717d12b29fa4269d648abf621f7
Partridge <anhduc130703@gmail.com>
2023-08-10 03:07:38 +0700

    fix link

--- src/app/api/link/route.ts
+++ src/app/api/link/route.ts
@@ -8,36 +8,41 @@ export async function GET(req: Request) {
   const href = url.searchParams.get(`url`);
   if (!href) return new Response('Invalid href', { status: 400 });
 
-  const { data } = await axios.get(href);
+  try {
+    const { data } = await axios.get(href);
+    console.log(data);
 
-  const descriptionMatch = data.match(
-    /<meta name="description" content="(.*?)"/
-  );
-  const description = descriptionMatch ? descriptionMatch[1] : '';
+    const descriptionMatch = data.match(
+      /<meta name="description" content="(.*?)"/
+    );
+    const description = descriptionMatch ? descriptionMatch[1] : '';
 
-  let title, titleMatch;
-  if (href.match(discordRegex)) {
-    titleMatch = data.match(/<meta property="og:title" content="(.*?)"/);
-    title = titleMatch ? titleMatch[1] : '';
-  } else {
-    titleMatch = data.match(/<title>(.*?)<\/title>/);
-    title = titleMatch ? titleMatch[1] : '';
-  }
+    let title, titleMatch;
+    if (href.match(discordRegex)) {
+      titleMatch = data.match(/<meta property="og:title" content="(.*?)"/);
+      title = titleMatch ? titleMatch[1] : '';
+    } else {
+      titleMatch = data.match(/<title>(.*?)<\/title>/);
+      title = titleMatch ? titleMatch[1] : '';
+    }
 
-  const imageMatch = data.match(/<meta property="og:image" content="(.*?)"/);
-  const imageUrl = imageMatch ? imageMatch[1] : '';
+    const imageMatch = data.match(/<meta property="og:image" content="(.*?)"/);
+    const imageUrl = imageMatch ? imageMatch[1] : '';
 
-  return new Response(
-    JSON.stringify({
-      success: 1,
-      link: href,
-      meta: {
-        title,
-        description,
-        image: {
-          url: imageUrl,
+    return new Response(
+      JSON.stringify({
+        success: 1,
+        link: href,
+        meta: {
+          title,
+          description,
+          image: {
+            url: imageUrl,
+          },
         },
-      },
-    })
-  );
+      })
+    );
+  } catch (error) {
+    return new Response('Something went wrong', { status: 500 });
+  }
 }

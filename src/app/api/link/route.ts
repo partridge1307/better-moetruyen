1566adc4e19e96cec4fe3bbd03b5436fb38a236f
Partridge <anhduc130703@gmail.com>
2023-08-10 03:49:46 +0700

    change fetch api link

--- src/app/api/link/route.ts
+++ src/app/api/link/route.ts
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 const discordRegex =
   /(https:\/\/)?(www)?discord.?(gg|com)?\/(invite)?\/([^\/\?\&\%]*)\S/;
 
@@ -9,28 +7,29 @@ export async function GET(req: Request) {
   if (!href) return new Response('Invalid href', { status: 400 });
 
   try {
-    const { data } = await axios.get(href);
-    const response = await fetch(href, {
-      method: 'GET',
-    });
-    console.log(await response.text());
-
-    const descriptionMatch = data.match(
-      /<meta name="description" content="(.*?)"/
-    );
-    const description = descriptionMatch ? descriptionMatch[1] : '';
+    const data = await (
+      await fetch(href, {
+        method: 'GET',
+        headers: { Accept: 'text/html; charset=utf-8' },
+      })
+    ).text();
 
     let title, titleMatch;
     if (href.match(discordRegex)) {
       titleMatch = data.match(/<meta property="og:title" content="(.*?)"/);
-      title = titleMatch ? titleMatch[1] : '';
+      title = titleMatch ? titleMatch[1] : null;
     } else {
       titleMatch = data.match(/<title>(.*?)<\/title>/);
-      title = titleMatch ? titleMatch[1] : '';
+      title = titleMatch ? titleMatch[1] : null;
     }
 
+    const descriptionMatch = data.match(
+      /<meta name="description" content="(.*?)"/
+    );
+    const description = descriptionMatch ? descriptionMatch[1] : null;
+
     const imageMatch = data.match(/<meta property="og:image" content="(.*?)"/);
-    const imageUrl = imageMatch ? imageMatch[1] : '';
+    const imageUrl = imageMatch ? imageMatch[1] : null;
 
     return new Response(
       JSON.stringify({
@@ -46,6 +45,6 @@ export async function GET(req: Request) {
       })
     );
   } catch (error) {
-    return new Response('Something went wrong', { status: 500 });
+    return new Response(JSON.stringify({ success: 0 }));
   }
 }

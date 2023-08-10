e7063e9dc71faf391a668328025ac4650e8ce049
Partridge <anhduc130703@gmail.com>
2023-08-11 05:56:52 +0700

    change manga description display

--- src/components/EditorOutput.tsx
+++ src/components/EditorOutput.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-import { cn } from '@/lib/utils';
-import { ChevronsDown, ChevronsUp } from 'lucide-react';
 import dynamic from 'next/dynamic';
-import { FC, useEffect, useRef, useState } from 'react';
+import { FC } from 'react';
 import CustomImage from './Renderer/CustomImage';
 import CustomLink from './Renderer/CustomLink';
 const Output = dynamic(
@@ -23,48 +21,33 @@ interface EditorOutputProps {
 }
 
 const EditorOutput: FC<EditorOutputProps> = ({ data }) => {
-  const ref = useRef<HTMLDivElement>(null);
-  const [showMore, setShowMore] = useState<boolean>(false);
-  const [isCollapsed, setIsCollapsed] = useState<boolean>(true);
-
-  useEffect(() => {
-    if (ref.current?.clientHeight && ref.current.clientHeight === 80) {
-      setShowMore(true);
-    }
-  }, [ref.current?.clientHeight]);
-
   return (
-    <div
-      ref={ref}
-      className={cn(
-        'relative max-h-20 overflow-hidden dark:bg-zinc-700 p-2 rounded-md',
-        !isCollapsed && 'max-h-fit pb-10'
-      )}
-    >
+    <div className="relative max-h-40 overflow-auto dark:bg-zinc-700 p-2 rounded-md">
       <Output data={data} renderers={renderers} />
-      {showMore ? (
-        <div
-          role="button"
-          onClick={() => setIsCollapsed((prev) => !prev)}
-          className="absolute inset-x-0 bottom-0 bg-gradient-to-t dark:from-zinc-900 rounded-md flex items-center justify-center"
-        >
-          {isCollapsed ? (
-            <>
-              <ChevronsDown className="w-4 h-4" />
-              Xem thêm
-              <ChevronsDown className="w-4 h-4" />
-            </>
-          ) : (
-            <>
-              <ChevronsUp className="w-4 h-4" />
-              Thu gọn
-              <ChevronsUp className="w-4 h-4" />
-            </>
-          )}
-        </div>
-      ) : null}
     </div>
   );
 };
 
 export default EditorOutput;
+
+// {showMore ? (
+//   <div
+//     role="button"
+//     onClick={() => setIsCollapsed((prev) => !prev)}
+//     className="absolute inset-x-0 bottom-0 bg-gradient-to-t dark:from-zinc-900 rounded-md flex items-center justify-center"
+//   >
+//     {isCollapsed ? (
+//       <>
+//         <ChevronsDown className="w-4 h-4" />
+//         Xem thêm
+//         <ChevronsDown className="w-4 h-4" />
+//       </>
+//     ) : (
+//       <>
+//         <ChevronsUp className="w-4 h-4" />
+//         Thu gọn
+//         <ChevronsUp className="w-4 h-4" />
+//       </>
+//     )}
+//   </div>
+// ) : null}

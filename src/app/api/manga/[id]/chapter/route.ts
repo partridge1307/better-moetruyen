d697aaa6d87fe24fd1bf35264060c3c43f41ed1a
Partridge <anhduc130703@gmail.com>
2023-08-12 21:15:38 +0700

    remove console log

--- src/app/api/manga/[id]/chapter/route.ts
+++ src/app/api/manga/[id]/chapter/route.ts
@@ -114,7 +114,6 @@ export async function POST(
 
     return new Response('OK');
   } catch (error) {
-    console.log(error);
     if (error instanceof z.ZodError)
       return new Response(error.message, { status: 422 });
     if (error instanceof Prisma.PrismaClientKnownRequestError) {

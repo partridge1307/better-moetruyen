302c2785d95099dd436da35eafc51246c4d6282d
Partridge <anhduc130703@gmail.com>
2023-09-02 20:50:20 +0700

    add padding to skeleton

--- src/components/Skeleton/LastestMangaSkeleton.tsx
+++ src/components/Skeleton/LastestMangaSkeleton.tsx
@@ -1,34 +1,34 @@
 const LastestMangaSkeleton = () => {
   return (
     <div className="space-y-3">
-      <div className="grid grid-cols-[.6fr_1fr] lg:grid-cols-[.3fr_1fr] rounded-md animate-pulse dark:bg-zinc-900">
+      <div className="grid grid-cols-[.6fr_1fr] lg:grid-cols-[.3fr_1fr] p-2 rounded-md animate-pulse dark:bg-zinc-900">
         <div style={{ aspectRatio: 4 / 3 }} />
       </div>
-      <div className="grid grid-cols-[.6fr_1fr] lg:grid-cols-[.3fr_1fr] rounded-md animate-pulse dark:bg-zinc-900">
+      <div className="grid grid-cols-[.6fr_1fr] lg:grid-cols-[.3fr_1fr] p-2 rounded-md animate-pulse dark:bg-zinc-900">
         <div style={{ aspectRatio: 4 / 3 }} />
       </div>
-      <div className="grid grid-cols-[.6fr_1fr] lg:grid-cols-[.3fr_1fr] rounded-md animate-pulse dark:bg-zinc-900">
+      <div className="grid grid-cols-[.6fr_1fr] lg:grid-cols-[.3fr_1fr] p-2 rounded-md animate-pulse dark:bg-zinc-900">
         <div style={{ aspectRatio: 4 / 3 }} />
       </div>
-      <div className="grid grid-cols-[.6fr_1fr] lg:grid-cols-[.3fr_1fr] rounded-md animate-pulse dark:bg-zinc-900">
+      <div className="grid grid-cols-[.6fr_1fr] lg:grid-cols-[.3fr_1fr] p-2 rounded-md animate-pulse dark:bg-zinc-900">
         <div style={{ aspectRatio: 4 / 3 }} />
       </div>
-      <div className="grid grid-cols-[.6fr_1fr] lg:grid-cols-[.3fr_1fr] rounded-md animate-pulse dark:bg-zinc-900">
+      <div className="grid grid-cols-[.6fr_1fr] lg:grid-cols-[.3fr_1fr] p-2 rounded-md animate-pulse dark:bg-zinc-900">
         <div style={{ aspectRatio: 4 / 3 }} />
       </div>
-      <div className="grid grid-cols-[.6fr_1fr] lg:grid-cols-[.3fr_1fr] rounded-md animate-pulse dark:bg-zinc-900">
+      <div className="grid grid-cols-[.6fr_1fr] lg:grid-cols-[.3fr_1fr] p-2 rounded-md animate-pulse dark:bg-zinc-900">
         <div style={{ aspectRatio: 4 / 3 }} />
       </div>
-      <div className="grid grid-cols-[.6fr_1fr] lg:grid-cols-[.3fr_1fr] rounded-md animate-pulse dark:bg-zinc-900">
+      <div className="grid grid-cols-[.6fr_1fr] lg:grid-cols-[.3fr_1fr] p-2 rounded-md animate-pulse dark:bg-zinc-900">
         <div style={{ aspectRatio: 4 / 3 }} />
       </div>
-      <div className="grid grid-cols-[.6fr_1fr] lg:grid-cols-[.3fr_1fr] rounded-md animate-pulse dark:bg-zinc-900">
+      <div className="grid grid-cols-[.6fr_1fr] lg:grid-cols-[.3fr_1fr] p-2 rounded-md animate-pulse dark:bg-zinc-900">
         <div style={{ aspectRatio: 4 / 3 }} />
       </div>
-      <div className="grid grid-cols-[.6fr_1fr] lg:grid-cols-[.3fr_1fr] rounded-md animate-pulse dark:bg-zinc-900">
+      <div className="grid grid-cols-[.6fr_1fr] lg:grid-cols-[.3fr_1fr] p-2 rounded-md animate-pulse dark:bg-zinc-900">
         <div style={{ aspectRatio: 4 / 3 }} />
       </div>
-      <div className="grid grid-cols-[.6fr_1fr] lg:grid-cols-[.3fr_1fr] rounded-md animate-pulse dark:bg-zinc-900">
+      <div className="grid grid-cols-[.6fr_1fr] lg:grid-cols-[.3fr_1fr] p-2 rounded-md animate-pulse dark:bg-zinc-900">
         <div style={{ aspectRatio: 4 / 3 }} />
       </div>
     </div>

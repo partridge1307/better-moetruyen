610647e7e8abbb13eda9a6d73fc0e63eb823926a
partridge <anhduc130703@gmail.com>
2023-09-24 13:19:05 +0700

    change social style

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -2,7 +2,7 @@
 
 import { cn } from '@/lib/utils';
 import styles from '@/styles/social.module.css';
-import { ArrowRight } from 'lucide-react';
+import { ArrowLeft, ArrowRight } from 'lucide-react';
 import { FC, useEffect, useState } from 'react';
 import { Icons } from './Icons';
 
@@ -47,21 +47,19 @@ const Social: FC<SocialProps> = ({ pageLikes, serverMembers }) => {
         <div className="relative aspect-video">
           <div className={styles.leftWrapper} />
 
-          <p className="absolute top-0 lg:top-[15%] right-0 rotate-6 font-semibold">
+          <p className="absolute top-0.5 lg:top-[15%] right-0 rotate-6 font-semibold">
             {pageLikes} lượt thích
           </p>
 
           <a
             href="https://www.facebook.com/Bfangteam"
             target="_blank"
-            className={`absolute inset-0 flex items-end ${styles.colorTransition}`}
-            style={{
-              clipPath: 'polygon(0 73%, 100% 100%, 0 100%)',
-            }}
+            className={`absolute inset-x-0 h-[30%] left-0 bottom-0 flex items-center ${styles.colorTransition}`}
+            style={{ clipPath: 'polygon(0% 100%, 100% 100%, 0 10%)' }}
           >
-            <p className="text-xl font-semibold inline-flex items-center gap-2">
-              Đến xem
-              <ArrowRight />
+            <p className="text-lg lg:text-6xl font-semibold lg:translate-x-1/2 translate-y-1/4 lg:translate-y-1/2 inline-flex items-center gap-2 rotate-6 lg:rotate-3">
+              <ArrowLeft className="lg:w-14 lg:h-14" />
+              <span>Đến xem</span>
             </p>
           </a>
         </div>
@@ -82,21 +80,19 @@ const Social: FC<SocialProps> = ({ pageLikes, serverMembers }) => {
         <div className="relative aspect-video">
           <div className={styles.rightWrapper} />
 
-          <p className="absolute top-0 left-0 lg:top-[15%] font-semibold -rotate-6">
+          <p className="absolute top-0.5 left-0 lg:top-[15%] font-semibold -rotate-6">
             {serverMembers} thành viên
           </p>
 
           <a
             href="https://www.facebook.com/Bfangteam"
             target="_blank"
-            className={`absolute inset-0 flex justify-end items-end ${styles.colorTransition}`}
-            style={{
-              clipPath: 'polygon(0 100%, 100% 73%, 100% 100%, 0 100%)',
-            }}
+            className={`absolute inset-x-0 h-[30%] left-0 bottom-0 flex justify-end items-center ${styles.colorTransition}`}
+            style={{ clipPath: 'polygon(0% 100%, 100% 100%, 100% 10%)' }}
           >
-            <p className="text-xl font-semibold inline-flex items-center gap-2">
-              Đến xem
-              <ArrowRight />
+            <p className="text-lg lg:text-6xl font-semibold lg:-translate-x-1/2 translate-y-1/4 lg:translate-y-1/2 inline-flex items-center gap-2 -rotate-6 lg:-rotate-3">
+              <span>Đến xem</span>
+              <ArrowRight className="lg:w-14 lg:h-14" />
             </p>
           </a>
         </div>

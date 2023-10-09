5bfaa370588f3e5d0ce3daaa346d6c49084a4e58
partridge <anhduc130703@gmail.com>
2023-10-10 01:56:26 +0700

    fix skeleton

--- src/components/Skeleton/NotableMangaSkeleton.tsx
+++ src/components/Skeleton/NotableMangaSkeleton.tsx
@@ -1,7 +1,8 @@
 const NotableMangaSkeleton = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-[.3fr_1fr] p-2 max-sm:pb-10 rounded-md animate-pulse dark:bg-zinc-900">
+    <div className="grid grid-cols-1 md:grid-cols-[.3fr_1fr] p-2 max-sm:pb-10 rounded-md animate-pulse bg-background">
       <div style={{ aspectRatio: 4 / 3 }} />
+      <div className="h-24" />
     </div>
   );
 };

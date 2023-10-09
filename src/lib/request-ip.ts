99cfa5e762e2f59aed421c3b37d2b85d74f158a9
partridge <anhduc130703@gmail.com>
2023-10-10 02:14:52 +0700

    change order request ip

--- src/lib/request-ip.ts
+++ src/lib/request-ip.ts
@@ -4,13 +4,13 @@ export const requestIp = (req: NextRequest) => {
   const ip =
     req.ip ||
     req.headers.get('x-client-ip') ||
-    req.headers.get('x-real-ip') ||
-    req.headers.get('cf-connecting-ip') ||
     req.headers.get('x-forwarded-for') ||
+    req.headers.get('cf-connecting-ip') ||
     req.headers.get('true-client-ip') ||
+    req.headers.get('x-real-ip') ||
     req.headers.get('x-forwarded') ||
-    req.headers.get('forwarded-for') ||
     req.headers.get('forwarded') ||
+    req.headers.get('forwarded-for') ||
     '127.0.0.1';
 
   return ip;

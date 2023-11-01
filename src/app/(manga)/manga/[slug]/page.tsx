ef9b957550db77a4e1e90a7e298dfdaa3bd2b1ba
partridge <anhduc130703@gmail.com>
2023-11-01 12:09:24 +0700

    optimize manga info

--- src/app/(manga)/manga/[slug]/page.tsx
+++ src/app/(manga)/manga/[slug]/page.tsx
@@ -1,25 +1,16 @@
 import ChapterList from '@/components/Chapter/ChapterList';
 import MangaImage from '@/components/Manga/components/MangaImage';
+import { MangaSubInfoSkeleton } from '@/components/Manga/components/MangaSubInfo';
 import DescriptionSkeleton from '@/components/Skeleton/DescriptionSkeleton';
-import UserAvatar from '@/components/User/UserAvatar';
-import UserBanner from '@/components/User/UserBanner';
-import Username from '@/components/User/Username';
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from '@/components/ui/Accordion';
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/components/ui/Popover';
 import { TagContent, TagWrapper } from '@/components/ui/Tag';
 import { db } from '@/lib/db';
-import { nFormatter } from '@/lib/utils';
 import type { Manga } from '@prisma/client';
-import { Bookmark, Eye, MessageSquare, Pen } from 'lucide-react';
 import type { Metadata } from 'next';
 import dynamic from 'next/dynamic';
 import Image from 'next/image';
@@ -27,6 +18,12 @@ import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import { FC } from 'react';
 
+const MangaSubInfo = dynamic(
+  () => import('@/components/Manga/components/MangaSubInfo'),
+  {
+    loading: () => <MangaSubInfoSkeleton />,
+  }
+);
 const MangaAction = dynamic(() => import('@/components/Manga/MangaAction'));
 const DiscordEmbed = dynamic(() => import('@/components/DiscordEmbed'));
 const FacebookEmbed = dynamic(() => import('@/components/FacebookEmbed'));
@@ -62,10 +59,6 @@ const page: FC<pageProps> = async ({ params }) => {
       discordLink: true,
       creator: {
         select: {
-          name: true,
-          color: true,
-          image: true,
-          banner: true,
           manga: {
             take: 3,
             where: {
@@ -92,22 +85,11 @@ const page: FC<pageProps> = async ({ params }) => {
           name: true,
         },
       },
-      view: {
-        select: {
-          totalView: true,
-        },
-      },
       tags: {
         select: {
           id: true,
           name: true,
           description: true,
-        },
-      },
-      _count: {
-        select: {
-          followedBy: true,
-          comment: true,
         },
       },
     },
@@ -157,51 +139,7 @@ const page: FC<pageProps> = async ({ params }) => {
               {manga.name}
             </h1>
 
-            <ul className="flex flex-wrap gap-3 md:gap-4">
-              <li className="max-sm:order-last">
-                <Popover modal>
-                  <PopoverTrigger className="max-sm:text-sm p-0.5 px-2 flex items-center gap-1.5 rounded-full ring-2 ring-foreground">
-                    <Pen className="w-4 h-4 md:w-5 md:h-5" />
-                    <span className="line-clamp-1">{manga.creator.name}</span>
-                  </PopoverTrigger>
-
-                  <PopoverContent className="p-1.5" asChild>
-                    <Link
-                      href={`/user/${manga.creator.name?.split(' ').join('-')}`}
-                      className="block"
-                    >
-                      <div className="relative">
-                        <UserBanner user={manga.creator} />
-                        <UserAvatar
-                          user={manga.creator}
-                          className="w-14 h-14 absolute left-4 bottom-0 translate-y-1/2 bg-background ring-4 ring-secondary"
-                        />
-                      </div>
-
-                      <Username
-                        user={manga.creator}
-                        className="text-start mt-11 pl-4"
-                      />
-                    </Link>
-                  </PopoverContent>
-                </Popover>
-              </li>
-
-              <li className="flex items-center gap-1.5">
-                <Bookmark className="w-5 h-5" />{' '}
-                {nFormatter(manga._count.followedBy, 1)}
-              </li>
-
-              <li className="flex items-center gap-1.5">
-                <Eye className="w-5 h-5" />{' '}
-                {nFormatter(manga.view?.totalView!, 1)}
-              </li>
-
-              <li className="flex items-center gap-1.5">
-                <MessageSquare className="w-5 h-5" />{' '}
-                {nFormatter(manga._count.comment, 1)}
-              </li>
-            </ul>
+            <MangaSubInfo mangaId={manga.id} />
           </div>
         </section>
 
---
+++ src/components/Manga/components/MangaSubInfo.tsx
@@ -0,0 +1,125 @@
+import UserAvatar from '@/components/User/UserAvatar';
+import UserBanner from '@/components/User/UserBanner';
+import Username from '@/components/User/Username';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/Popover';
+import { db } from '@/lib/db';
+import { nFormatter } from '@/lib/utils';
+import { Bookmark, Eye, Loader2, MessageSquare, Pen } from 'lucide-react';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
+import { FC } from 'react';
+
+interface MangaSubInfoProps {
+  mangaId: number;
+}
+
+const MangaSubInfo: FC<MangaSubInfoProps> = async ({ mangaId }) => {
+  const manga = await db.manga.findUnique({
+    where: {
+      id: mangaId,
+      isPublished: true,
+    },
+    select: {
+      creator: {
+        select: {
+          name: true,
+          color: true,
+          image: true,
+          banner: true,
+        },
+      },
+      view: {
+        select: {
+          totalView: true,
+        },
+      },
+      _count: {
+        select: {
+          followedBy: true,
+          comment: true,
+        },
+      },
+    },
+  });
+  if (!manga) return notFound();
+
+  return (
+    <ul className="flex flex-wrap gap-3 md:gap-4">
+      <li className="max-sm:order-last">
+        <Popover modal>
+          <PopoverTrigger className="max-sm:text-sm p-0.5 px-2 flex items-center gap-1.5 rounded-full ring-2 ring-foreground">
+            <Pen className="w-4 h-4 md:w-5 md:h-5" />
+            <span className="line-clamp-1">{manga.creator.name}</span>
+          </PopoverTrigger>
+
+          <PopoverContent className="p-1.5" asChild>
+            <Link
+              href={`/user/${manga.creator.name?.split(' ').join('-')}`}
+              className="block"
+            >
+              <div className="relative">
+                <UserBanner user={manga.creator} />
+                <UserAvatar
+                  user={manga.creator}
+                  className="w-14 h-14 absolute left-4 bottom-0 translate-y-1/2 bg-background ring-4 ring-secondary"
+                />
+              </div>
+
+              <Username
+                user={manga.creator}
+                className="text-start mt-11 pl-4"
+              />
+            </Link>
+          </PopoverContent>
+        </Popover>
+      </li>
+
+      <li className="flex items-center gap-1.5">
+        <Bookmark className="w-5 h-5" />
+        {nFormatter(manga._count.followedBy, 1)}
+      </li>
+
+      <li className="flex items-center gap-1.5">
+        <Eye className="w-5 h-5" /> {nFormatter(manga.view?.totalView!, 1)}
+      </li>
+
+      <li className="flex items-center gap-1.5">
+        <MessageSquare className="w-5 h-5" />
+        {nFormatter(manga._count.comment, 1)}
+      </li>
+    </ul>
+  );
+};
+
+export default MangaSubInfo;
+
+export const MangaSubInfoSkeleton = () => (
+  <ul className="flex flex-wrap gap-3 md:gap-4">
+    <li className="max-sm:order-last">
+      <button
+        aria-label="author info loading"
+        className="p-0.5 px-2 flex items-center gap-1.5 rounded-full ring-2 ring-foreground"
+      >
+        <Pen className="w-4 h-4 md:w-5 md:h-5" />
+        <Loader2 className="w-4 h-4 md:w-5 md:h-5 animate-spin" />
+      </button>
+    </li>
+
+    <li className="flex items-center gap-1.5">
+      <Bookmark className="w-5 h-5" />
+      <Loader2 className="w-4 h-4 md:w-5 md:h-5 animate-spin" />
+    </li>
+    <li className="flex items-center gap-1.5">
+      <Eye className="w-5 h-5" />
+      <Loader2 className="w-4 h-4 md:w-5 md:h-5 animate-spin" />
+    </li>
+    <li className="flex items-center gap-1.5">
+      <MessageSquare className="w-5 h-5" />
+      <Loader2 className="w-4 h-4 md:w-5 md:h-5 animate-spin" />
+    </li>
+  </ul>
+);

49dea144096366206bc44f6d1fdb836b65b6b1a4
partridge <anhduc130703@gmail.com>
2023-11-11 19:44:38 +0700

    change order of req ip

--- src/lib/request-ip.ts
+++ src/lib/request-ip.ts
@@ -2,11 +2,11 @@ import type { NextRequest } from 'next/server';
 
 export const requestIp = (req: NextRequest) => {
   const ip =
-    req.ip ||
     req.headers.get('x-client-ip') ||
     req.headers.get('x-forwarded-for') ||
     req.headers.get('cf-connecting-ip') ||
     req.headers.get('true-client-ip') ||
+    req.ip ||
     req.headers.get('x-real-ip') ||
     req.headers.get('x-forwarded') ||
     req.headers.get('forwarded-for') ||

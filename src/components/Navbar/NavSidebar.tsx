30134a4fef60db6fec71edc352b86c2b9cffe16b
partridge <anhduc130703@gmail.com>
2023-11-23 16:05:30 +0700

    expand homepage

--- src/components/Navbar/NavSidebar.tsx
+++ src/components/Navbar/NavSidebar.tsx
@@ -194,7 +194,7 @@ const NavSidebar = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
         <div className="flex h-[10%] items-center justify-between px-6 bg-primary-foreground">
           <div>
             <p>©Moetruyen</p>
-            <p>Version: 1.2.0</p>
+            <p>Version: 1.2.1</p>
           </div>
           <SwitchWithIcon checked={isChecked} onCheckedChange={handleSwitch}>
             <SunMoon />
